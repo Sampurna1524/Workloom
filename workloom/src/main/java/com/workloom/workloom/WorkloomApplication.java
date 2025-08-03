b3dce02a81272de03c317d2484639f761bf67e19
Sampurna Roy <sampurna.roycs@gmail.com>
2025-08-03 15:01:18 +0530

    Add files via upload

---
+++ workloom/src/main/java/com/workloom/workloom/WorkloomApplication.java
@@ -0,0 +1,13 @@
+package com.workloom.workloom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkloomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkloomApplication.class, args);
+	}
+
+}
---
+++ workloom/src/test/java/com/workloom/workloom/WorkloomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workloom.workloom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkloomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
